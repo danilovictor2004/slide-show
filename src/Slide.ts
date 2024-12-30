64f2290c5c6d365fc80aa56a7542058efc8adfdd
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2024-12-30 13:34:22 -0300

    cria base para slide-show

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,18 +1,31 @@
 export default class Slide {
   container: Element;
-  elements: Element[];
+  slides: Element[];
   controls: Element;
   time: number;
-  constructor(
-    container: Element,
-    elements: Element[],
-    controls: Element,
-    time: number = 5000
-  ) {
+  index: number;
+  slide: Element;
+
+  constructor(container: Element, slides: Element[], controls: Element, time: number = 5000) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
 
+    this.index = 2;
+    this.slide = this.slides[this.index];
+
+    this.show(this.index);
+  }
+
+  hide(el: Element) {
+    el.classList.remove("active");
+  }
+
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((el) => this.hide(el));
+    this.slide.classList.add("active");
   }
 }
