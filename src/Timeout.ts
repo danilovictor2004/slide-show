5016e814c50fffae00bf1206dfa81e47060fbbc7
Danillo Caetano <danillo.caetano@unimedmaceio.com.br>
2025-01-06 14:00:31 -0300

    Programa o pause e continue ao clicar na imagem do slide show

--- src/Timeout.ts
+++ src/Timeout.ts
@@ -1,14 +1,30 @@
 export default class Timeout {
     id;
     handler;
+    start;
+    timeLeft;
 
     constructor(handler: TimerHandler, time: number) {
         this.id = setTimeout(handler, time);
         this.handler = handler;
+        this.start = Date.now();
+        this.timeLeft = time
     }
 
-    clear() {
+    clear(): void {
         clearTimeout(this.id);
+    }
+
+    pause(): void {
+        const passed = Date.now() - this.start;
+        this.timeLeft = this.timeLeft - passed;
+        this.clear();
+    }
+
+    continue(): void {
+        this.clear();
+        this.id = setTimeout(this.handler, this.timeLeft);
+        this.start = Date.now();
     }
 
 }
